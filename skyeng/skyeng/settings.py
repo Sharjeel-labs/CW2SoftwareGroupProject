a6434fcffaf397332a1f64acb61fb699e3f824bf
Sharjeel-labs <sharcyber@gmail.com>
2026-04-09 14:57:02 +0100

    Added homepage template and inbox page

--- skyeng/skyeng/settings.py
+++ skyeng/skyeng/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # (Student 3 - Messages)
+    'messages_app',
 ]
 
 MIDDLEWARE = [
--- skyeng/skyeng/urls.py
+++ skyeng/skyeng/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('', home),  
+    path('messages_app/', include('messages_app.urls')),
 ]
