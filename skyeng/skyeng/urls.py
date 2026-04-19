0a22dc9e52e72b8062c39ed9593c3d611a0aa23a
Andrii Prykhno <andriiprykhno@MacBook-Pro.local>
2026-04-20 01:23:46 +0300

    feat: add schedule app

--- skyeng/skyeng/urls.py
+++ skyeng/skyeng/urls.py
@@ -1,6 +1,10 @@
+from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('', include('schedule_app.urls')),
 ]
