9729f6602988c4adb819225fe886927f99a9e95d
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-25 21:39:25 +0100

    checked other views and fixed so messages_app/ is same as messages/inbox may change

--- skyeng/messages_app/views.py
+++ skyeng/messages_app/views.py
@@ -1,5 +1,16 @@
-# Create your views here.
 from django.shortcuts import render
 
 def inbox(request):
     return render(request, 'messages_app/inbox.html')
+
+def compose(request):
+    return render(request, 'messages_app/compose.html')
+
+def sent(request):
+    return render(request, 'messages_app/sent.html')
+
+def drafts(request):
+    return render(request, 'messages_app/drafts.html')
+
+def trash(request):
+    return render(request, 'messages_app/trash.html')
