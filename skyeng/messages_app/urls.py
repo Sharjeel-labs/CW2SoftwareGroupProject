faf75a6bb9d383e20e1a046151eacb70a778ae04
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-25 21:35:25 +0100

    updated urls.py and    added trash

--- skyeng/messages_app/urls.py
+++ skyeng/messages_app/urls.py
@@ -1,6 +1,19 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
 from . import views
 
 urlpatterns = [
+    path('', views.inbox, name='inbox'),
     path('inbox/', views.inbox, name='inbox'),
+    path('compose/', views.compose, name='compose'),
+    path('sent/', views.sent, name='sent'),
+    path('drafts/', views.drafts, name='drafts'),
+    path('trash/', views.trash, name='trash'),
 ]
